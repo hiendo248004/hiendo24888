3f0e857ee88e799a972c9f551870af5e8e9a02d4
hiendo248004 <hiendo248004hehe@gmail.com>
2026-04-21 04:31:52 +0700

    Create Tour model file with methods for managing tours

---
+++ models/Tour.php
@@ -0,0 +1,33 @@
+<?php
+
+class Tour {
+    // Method to get all tours
+    public static function getAllTours() {
+        // Implement logic to get all tours
+    }
+
+    // Method to get a tour by ID
+    public static function getTourById($id) {
+        // Implement logic to get a tour by ID
+    }
+
+    // Method to search tours
+    public static function searchTours($criteria) {
+        // Implement logic to search tours based on criteria
+    }
+
+    // Method to create a new tour
+    public static function createTour($tourData) {
+        // Implement logic to create a new tour
+    }
+
+    // Method to update an existing tour
+    public static function updateTour($id, $tourData) {
+        // Implement logic to update a tour
+    }
+
+    // Method to delete a tour
+    public static function deleteTour($id) {
+        // Implement logic to delete a tour
+    }
+}
