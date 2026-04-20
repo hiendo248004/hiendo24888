f1e7c1df096cf27be6ec28576074c91f09129836
hiendo248004 <hiendo248004hehe@gmail.com>
2026-04-21 04:36:57 +0700

    Add PaymentController with payment processing methods

---
+++ controllers/PaymentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\BaseModel;
+
+class PaymentController extends BaseModel {
+    
+    public function processPayment($paymentData) {
+        // Logic for processing payment
+    }
+    
+    public function checkout($checkoutData) {
+        // Logic for checkout
+    }
+    
+    public function processCheckout($checkoutData) {
+        // Logic for processing checkout
+    }
+}
