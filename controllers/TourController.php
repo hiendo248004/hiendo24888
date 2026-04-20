dffb8cca7796cd6c5ab92823530daf8cd8326975
hiendo248004 <hiendo248004hehe@gmail.com>
2026-04-21 04:36:46 +0700

    Add TourController.php to manage tours

---
+++ controllers/TourController.php
@@ -0,0 +1,41 @@
+<?php
+
+class TourController extends BaseModel {
+
+    public function getAllTours() {
+        // Code to get all tours from the database
+    }
+
+    public function getTourById($id) {
+        // Code to get a tour by ID from the database
+    }
+
+    public function searchTours($criteria) {
+        // Code to search for tours based on criteria
+    }
+
+    public function createTour($tourData) {
+        // Code to create a new tour in the database
+    }
+
+    public function updateTour($id, $tourData) {
+        // Code to update an existing tour in the database
+    }
+
+    public function deleteTour($id) {
+        // Code to delete a tour from the database
+    }
+
+    public function index() {
+        // Code to display all tours
+tour data from database
+    }
+
+    public function detail($id) {
+        // Code to display details of a specific tour
+    }
+
+    public function search($query) {
+        // Code to perform a search on tours
+    }
+}
